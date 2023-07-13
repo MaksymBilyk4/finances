300dc5c547ccf15f649a5198e640f12e5a5ed794
Maksym Bilyk <maksym.bilyk.business@gmail.com>
2023-07-13 20:17:02 +0300

    field validation

--- client/src/pages/Expenses.jsx
+++ client/src/pages/Expenses.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, DatePicker, Input, InputNumber} from "antd";
+import {Button, DatePicker, Form, Input, InputNumber, message} from "antd";
 import {parseDate} from "../utils/parseDate";
 import {MinusCircleOutlined} from "@ant-design/icons";
 import {create} from "../api/expense_api";
@@ -9,10 +9,18 @@ const Expenses = () => {
     const [date, setDate] = useState("");
     const [cost, setCost] = useState(0.00);
     const [description, setDescription] = useState("");
+    const [messageApi, contextHolder] = message.useMessage();
 
     const onDateChange = val => setDate(val?.$d || "");
     const onCostChange = val => setCost(val);
     const onDescriptionChange = e => setDescription(e.target.value);
+
+    const success = () => {
+        messageApi.open({
+            type: 'success',
+            content: 'Покупку успішно створено',
+        });
+    };
 
     const onSubmit = () => {
         create({
@@ -24,43 +32,68 @@ const Expenses = () => {
         setDescription("");
         setCost(0);
         setDate("");
+
+        success();
     }
 
     return (
         <>
-            <div>
-                <p>Витрата: </p>
-                <Input size="large"
-                       placeholder="Введіть опис витрати"
-                       value={description}
-                       onChange={onDescriptionChange}
-                       prefix={<MinusCircleOutlined/>}
-                />
-            </div>
+            {contextHolder}
+            <Form onFinish={onSubmit}>
+                <Form.Item
+                    label="Витрата"
+                    rules={[{
+                        required: true,
+                        message: "Введіть опис витрати. На приклад: сміття, комуналка"
+                    }]}
+                    name="description"
+                >
+                    <Input
+                        size="large"
+                        placeholder="Введіть опис витрати"
+                        value={description}
+                        onChange={onDescriptionChange}
+                        prefix={<MinusCircleOutlined/>}
+                    />
+                </Form.Item>
 
-            <div style={{marginTop: "10px"}}>
-                <p>Дата: </p>
-                <DatePicker format="DD.MM.YYYY"
-                            onChange={onDateChange}
-                />
-            </div>
-
-            <div style={{marginTop: "10px"}}>
-                <p>Витрачена сума: </p>
-                <InputNumber addonAfter="UAH"
-                             defaultValue={0.00}
-                             value={cost}
-                             onChange={onCostChange}
-                />
-            </div>
-
-            <Button onClick={onSubmit}
+                <Form.Item
                     style={{marginTop: "10px"}}
-                    type="primary"
-                    disabled={date.length <= 0 || Number(cost) <= 0 || description.length <= 0}
-            >Зберегти</Button>
-        </>
+                    label={"Дата"}
+                    rules={[{
+                        required: true,
+                        message: "Виберіть дату покупки"
+                    }]}
+                    name="date"
+                >
+                    <DatePicker format="DD.MM.YYYY"
+                                onChange={onDateChange}
+                    />
+                </Form.Item>
 
+                <Form.Item
+                    style={{marginTop: "10px"}}
+                    label="Вартість покупки"
+                    rules={[{
+                        required: true,
+                        message: "Введіть вартість покупки"
+                    }]}
+                    name="cost"
+                >
+                    <InputNumber addonAfter="UAH"
+                                 defaultValue={0.00}
+                                 value={cost}
+                                 onChange={onCostChange}
+                    />
+                </Form.Item>
+
+                <Button htmlType={"submit"}
+                        style={{marginTop: "10px"}}
+                        type="primary"
+                    // disabled={date.length <= 0 || Number(cost) <= 0 || description.length <= 0}
+                >Зберегти</Button>
+            </Form>
+        </>
     );
 };
 
